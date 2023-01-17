f182ae0c77c485c666a5477e5938aef515b8e2a0
Jaris <john.aristosa@hotmail.com>
2023-01-17 12:10:48 +0100

    Symptômes page
    Nouveau style de la page symptome, elle demande maintenant l'age et le sexe avec un nouveau style pour chaque étapes. Changement de la page résultat avec un message de warning

--- lib/screens/result.dart
+++ lib/screens/result.dart
@@ -1,4 +1,6 @@
 import 'package:caducee/common/const.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -8,19 +10,71 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: myGreen, size: 30.0),
-          onPressed: () => Navigator.of(context).pop(),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: myGreen, size: 30.0),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
         ),
-        title: Text('Maladie potentielle', style: TextStyle(color: Theme.of(context).textTheme.bodyText1!.color)),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Center(
-          child: Text(response, style: TextStyle(color: Theme.of(context).textTheme.bodyText1!.color)),
-        ),
-      ),
-    );
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Résultat",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SizedBox(
+                  child: Column(
+                    children: [
+                      Text(
+                        response,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: const [
+                Icon(Icons.warning, color: Colors.red, size: 30.0),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Attention, ce texte à été généré par une IA, ne vous fiez pas à ce texte pour prendre une décision médicale. Consultez toujours un médecin en cas de doute.",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
--- lib/screens/symptoms.dart
+++ lib/screens/symptoms.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:caducee/common/const.dart';
 import 'package:caducee/screens/result.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 import 'package:http/http.dart' as http;
 
 const backgroundColor = Color(0xff343541);
@@ -53,26 +54,40 @@ class _SymptomsState extends State<Symptoms> {
   int currentStep = 0;
   String response = "";
   List<String> symptoms = [];
-  String _selectedValue = "";
-  final List<String> bouche = ["Aucun", "Douleur à la mâchoire", "maux de dents", "Saignements de gencives", "Aphtes", "Ulcères buccaux", "Langue enflée", "Mauvaise haleine"];
-  final List<String> gorge = ["Toux sèche", "Douleur en avalant", "Enrouement", "Douleur cervicale", "Ganglions lymphatiques enflés"];
-  final List<String> vue = ["Aucun",
-    "Vision floue", "Double vision", "Maux de tête liés à la vue", "Yeux rouges", "Éblouissement", "Photophobie"
-  ];
-  final List<String> oreilles = ["Aucun",
-    "Acouphènes", "Bourdonnements d'oreille", "Perte d'audition", "Douleur à l'oreille", "Vertiges", "Équilibre Instable"
-  ];
-  final List<String> nez = [ "Aucun",
-    "Nez qui coule", "Nez bouché", "Saignements de nez", "Perte d'odorat", "Démangeaisons de la narine"
-  ];
-  final List<String> tete = ["Aucun",
-    "Maux de tête", "Migraines", "Vertiges", "Étourdissements", "Perte de conscience", "Vomissements"
+  double _selectedAge = 0;
+  int _selectedGender = 0;
+  int age = 0;
+  String genre = "";
+
+  static final List<String> allSymptoms = [
+    "Fièvre",
+    "Fatigue",
+    "Nez bouché",
+    "Nez qui coule",
+    "Mal de gorge",
+    "Éternuements",
+    "Douleurs articulaires",
+    "Douleurs musculaires",
+    "Transpiration",
+    "Transpiration nocturne",
+    "Toux",
+    "Toux sèche",
+    "Toux grasse",
+    "Essoufflement",
+    "Perte de l'odorat",
+    "Perte du goût",
+    "Démangeaisons",
+    "Diarhée",
+    "Constipation",
+    "Œdème",
+    "Vomissements",
+    "Douleurs musculaires",
+    "Maux de tête"
 
   ];
-
-  final List<String> corps = ["Aucun", 
-     "Douleurs articulaires", "Douleurs musculaires", "Fatigue", "Frissons", "Transpiration", "Perte de poids", "Fièvre"
-  ];
+  final _items = allSymptoms
+      .map((animal) => MultiSelectItem<String>(animal, animal))
+      .toList();
 
   @override
   void initState() {
@@ -85,214 +100,114 @@ class _SymptomsState extends State<Symptoms> {
       Step(
         state: currentStep > 0 ? StepState.complete : StepState.indexed,
         isActive: true,
-        title: const Text('Bouche'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: bouche.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: bouche[index],
-                  groupValue: _selectedValue,
-                  title: Text(bouche[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => bouche.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                        _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-
-                    });
-                  },
-                );
+        title: const Text('Âge'),
+        content: Column(
+          children: [
+            const Text("Veuillez renseigner vôtre âge"),
+            Slider(
+              value: _selectedAge,
+              onChanged: (newValue) {
+                setState(() {
+                  _selectedAge = newValue;
+                  age = _selectedAge.round();
+                });
               },
-            )),
+              min: 0,
+              max: 100,
+              divisions: 100,
+              label: "${_selectedAge.round()} ans",
+            ),
+          ],
+        ),
       ),
       Step(
         state: currentStep > 1 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 1,
-        title: const Text('Gorge'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: gorge.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: gorge[index],
-                  groupValue: _selectedValue,
-                  title: Text(gorge[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => gorge.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                        _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-                    });
-                  },
-                );
+        isActive: true,
+        title: const Text('Genre'),
+        content: Column(
+          children: [
+            const Text("Veuillez renseigner vôtre genre"),
+            RadioListTile(
+              title: const Text('Homme'),
+              value: 0,
+              groupValue: _selectedGender,
+              onChanged: (value) {
+                setState(() {
+                  genre = "homme";
+                  _selectedGender = value!;
+                });
               },
-            )),
+            ),
+            RadioListTile(
+              title: const Text('Femme'),
+              value: 1,
+              groupValue: _selectedGender,
+              onChanged: (value) {
+                setState(() {
+                  _selectedGender = value!;
+                  genre = "femme";
+                });
+              },
+            ),
+          ],
+        ),
       ),
       Step(
-        state: currentStep > 2 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 2,
-        title: const Text('Vue'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: vue.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: vue[index],
-                  groupValue: _selectedValue,
-                  title: Text(vue[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => vue.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                        _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-                    });
-                  },
-                );
-              },
-            )),
-      ),
-      Step(
-        state: currentStep > 3 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 3,
-        title: const Text('Oreilles'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: oreilles.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: oreilles[index],
-                  groupValue: _selectedValue,
-                  title: Text(oreilles[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => oreilles.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                        _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-                    });
-                  },
-                );
-              },
-            )),
-      ),
-      Step(
-        state: currentStep > 4 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 4,
-        title: const Text('Nez'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: nez.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: nez[index],
-                  groupValue: _selectedValue,
-                  title: Text(nez[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => nez.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                        _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-                    });
-                  },
-                );
-              },
-            )),
-      ),
-      Step(
-        state: currentStep > 5 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 5,
-        title: const Text('Tête'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: tete.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: tete[index],
-                  groupValue: _selectedValue,
-                  title: Text(tete[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => tete.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                        _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-                    });
-                  },
-                );
-              },
-            )),
-      ),
-      Step(
-        state: currentStep > 6 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 6,
-        title: const Text('Corps'),
-        content: SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: ListView.builder(
-              itemCount: corps.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RadioListTile(
-                  value: corps[index],
-                  groupValue: _selectedValue,
-                  title: Text(corps[index]),
-                  onChanged: (value) {
-                    setState(() {
-                      symptoms
-                          .removeWhere((symptom) => corps.contains(symptom));
-                      symptoms.add(value!);
-                      if (value == "Aucun") {
-                       _selectedValue = "";
-                      } else {
-                        _selectedValue = value;
-                      }
-                    });
-                  },
-                );
-              },
-            )),
-      ),
+          title: const Text('Symptômes'),
+          content: Column(
+            children: [
+              SingleChildScrollView(
+                  child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        children: <Widget>[
+                          const SizedBox(height: 40),
+                          MultiSelectDialogField(
+                            searchable: true,
+                            items: _items,
+                            title: const Text("Symptômes"),
+                            selectedColor: myDarkGreen,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                              border: Border.all(
+                                color: myGreen,
+                                width: 1,
+                              ),
+                            ),
+                            buttonIcon: const Icon(
+                              Icons.thermostat_outlined,
+                              color: myDarkGreen,
+                            ),
+                            buttonText: const Text(
+                              "Selectionner les symptômes",
+                              style: TextStyle(
+                                color: myDarkGreen,
+                                fontSize: 16,
+                              ),
+                            ),
+                            confirmText: const Text("Confirmer",
+                                style: TextStyle(color: myDarkGreen)),
+                            cancelText: const Text("Annuler",
+                                style: TextStyle(color: myDarkGreen)),
+                                isDismissible: true,
+                                validator: (value) {
+                                  if (value == null || value.length < 2) {
+                                    return 'Veuillez sélectionner au moins 2 symptôme';
+                                  }
+                                  if (value.length > 6) {
+                                    return 'Veuillez sélectionner au plus 6 symptômes';
+                                  }
+                                },
+                            onConfirm: (value) {
+                              symptoms = value;
+                            },
+                          ),
+                        ],
+                      )))
+            ],
+          )),
     ];
   }
 
@@ -313,7 +228,7 @@ class _SymptomsState extends State<Symptoms> {
                   : Center(
                       child: Image.asset('assets/images/diagnostic_light.gif'))
               : Stepper(
-                  type: StepperType.vertical,
+                  type: StepperType.horizontal,
                   steps: getSteps(),
                   currentStep: currentStep,
                   onStepTapped: (value) => setState(() => currentStep = value),
@@ -359,7 +274,7 @@ class _SymptomsState extends State<Symptoms> {
                                     // print(symptoms);
                                   });
                                   response = await generateResponse(
-                                      "J'ai ces symptômes : ${symptoms.join(", ")}. Fais une phrase dans ce style :Vous avez potentiellement: (nom des maladies). (conseil d'aller voir un médecin).");
+                                      "J'ai $age ans et je suis un(e) $genre et j'ai ces symptômes : ${symptoms.join(", ")}. Fais une phrase dans ce style :Vous avez potentiellement: (nom des maladies). (conseil d'aller voir un médecin)");
                                   _showResponsePage(response);
                                   setState(() {
                                     isLoading = false;
